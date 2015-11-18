c934c8bae6dcfd1bbfba0efb34bbfebf3c164419
BEC1 <bec1mailbox@gmail.com>
2015-11-18 17:11:18 -0500

    Revert "Convert the data matrix into a atom number map"
    This reverts commit cb12b4a2dba797797efe03f6715bdc136a178298.

--- Topimg/fitsreadRL.m
+++
@@ -1,8 +0,0 @@
-function img = fitsreadRL( filename )
-%FITSREADRL Summary of this function goes here
-%   Detailed explanation goes here
-copyfile(filename,'temp.fits');
-img=fitsread('temp.fits');
-delete('temp.fits');
-end
-
